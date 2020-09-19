5b85b18d9cebec488ce476173a59d93825c414a0
DarkShadows <471583744@qq.com>
2020-09-19 23:04:14 +0800

    番茄工作-未完成

--- app.js
+++ app.js
@@ -6,5 +6,20 @@ App({
       // 用户信息保存到访问用户记录
       traceUser:true
     })
+    // 下面闹钟
+    let workTime = wx.getStorageSync('workTime')
+    let restTime = wx.getStorageSync('restTime')
+    if (!workTime) {
+      wx.setStorage({
+        key: 'workTime',
+        data: defaultTime.defaultWorkTime
+      })
+    }
+    if (!restTime) {
+      wx.setStorage({
+        key: 'restTime',
+        data: defaultTime.defaultRestTime
+      })
+    }
   }
 })
