140886bbdca87932dfe3adc6950e442a3839bb44
DarkShadows <471583744@qq.com>
2020-09-22 10:58:59 +0800

    下拉刷新，计时器

--- pages/home/home.js
+++ pages/home/home.js
@@ -13,54 +13,67 @@ Page({
   
   }, 
  
-  
-  
+  onPullDownRefresh(){
+    　　console.log('--------下拉刷新-------')
+    　　wx.showNavigationBarLoading() //在标题栏中显示加载
+        setTimeout(wx.stopPullDownRefresh(),2000)
+    　　this.getshow()
+        
+  },
+
   onShow:function(e){
-    // 获取缓存信息
-    const ui = wx.getStorageSync("userinfo")
-    
-    const that = this
-    
-    wx.cloud.callFunction({
-      name:'getlist',
-        // 上传
-        data: {
-          openid:ui.openid
-        }, 
-        // 下传
-        success:res=>{
-          that.setData({
-            list:res.result.data.map(list=>{
-              var date = util.formatTime(new Date(list.date))
-              list.date = date
-              return list
+    this.getshow()
+  },
+
+  getshow:function(){
+   
+      // 获取缓存信息
+      const ui = wx.getStorageSync("userinfo")
+      
+      const that = this
+      
+      wx.cloud.callFunction({
+        name:'getlist',
+          // 上传
+          data: {
+            openid:ui.openid
+          }, 
+          // 下传
+          success:res=>{
+            that.setData({
+              list:res.result.data.map(list=>{
+                var date = util.formatTime(new Date(list.date))
+                list.date = date
+                return list
+              })
             })
-          })
+            },
+          fail:res=>{
+            console.log("res获取失败",res)
+            }
+      })
+    
+      wx.cloud.callFunction({
+        name:'getnotepad',
+          // 上传
+          data: {
+            openid:ui.openid
           },
-        fail:res=>{
-          console.log("res获取失败",res)
-          }
-    })
-  
-    wx.cloud.callFunction({
-      name:'getnotepad',
-        // 上传
-        data: {
-          openid:ui.openid
-        },
-        // 下传
-        success:res=>{
-          that.setData({
-            notepad:res.result.data.map(notepad=>{
-              var date = util.formatTime(new Date(notepad.date))
-              notepad.date = date
-              return notepad
+          // 下传
+          success:res=>{
+            that.setData({
+              notepad:res.result.data.map(notepad=>{
+                var date = util.formatTime(new Date(notepad.date))
+                notepad.date = date
+                return notepad
+              })
             })
-          })
-          },
-        fail:res=>{
-          console.log("res获取失败",res)
-          }
-    })
+            },
+          fail:res=>{
+            console.log("res获取失败",res)
+            }
+      })
+    
   }
+
 }) 
