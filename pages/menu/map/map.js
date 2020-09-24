51a94b27629998f056035eb3f69b9144bb79cc69
DarkShadows <471583744@qq.com>
2020-09-24 11:35:55 +0800

    增加地图天气功能

---
+++ pages/menu/map/map.js
@@ -0,0 +1,63 @@
+// 引用百度地图微信小程序JSAPI模块 
+var bmap = require('../../../libs/bmap-wx.js'); 
+var wxMarkerData = []; 
+Page({ 
+    data: { 
+        markers: [], 
+        latitude: '', 
+        longitude: '', 
+        rgcData: {} 
+    }, 
+    // 刷新
+    refresh:function(){
+        this.onLoad()
+    },
+    makertap: function(e) { 
+        var that = this; 
+        var id = e.markerId; 
+        that.showSearchInfo(wxMarkerData, id); 
+    }, 
+    onLoad: function() { 
+        var that = this; 
+        // 新建百度地图对象 
+        var BMap = new bmap.BMapWX({ 
+            ak: 'cjbWp9jDRp6Vul58yvWX4h6PoG9rUzlH' 
+        }); 
+        var fail = function(data) { 
+            console.log(data) 
+        }; 
+        var success = function(data) { 
+            wxMarkerData = data.wxMarkerData; 
+            that.setData({ 
+                markers: wxMarkerData 
+            }); 
+            that.setData({ 
+                latitude: wxMarkerData[0].latitude 
+            }); 
+            that.setData({ 
+                longitude: wxMarkerData[0].longitude 
+            }); 
+        } 
+        // 发起regeocoding检索请求 
+        BMap.regeocoding({ 
+            fail: fail, 
+            success: success, 
+            iconPath: '../../../icon-img/marker_red.png', 
+            iconTapPath: '../../../icon-img/marker_red.png' 
+        }); 
+    }, 
+    showSearchInfo: function(data, i) { 
+        var that = this; 
+        that.setData({ 
+            rgcData: { 
+                address: '地址：' + data[i].address + '\n', 
+                desc: '描述：' + data[i].desc + '\n', 
+                business: '商圈：' + data[i].business 
+            } 
+        })
+        console.log('地图信息',that.data)
+        ; 
+        this.showSearchInfo()
+    } 
+
+})
