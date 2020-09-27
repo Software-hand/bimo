638bd53a72b9fb2e2de7116f3e1d160801f1bb8a
DarkShadows <471583744@qq.com>
2020-09-27 14:25:51 +0800

    内容审核

--- pages/add/list/list.js
+++ pages/add/list/list.js
@@ -3,75 +3,111 @@ Page({
     // 内容
     listdata:'',
     // 用户openid
-    openid:''
+    openid:'',
+    // 审核
+    errCode:'',
+    // 审核信息
+    errMsg:''
   },
-
+ 
   addlist: function(e) {
     console.log("失去焦点",e)
   },
 
   // 保存待办事项
   add1: function(e){
+    
     this.setData({
       listdata:e.detail.value
     })
+    
     console.log("保存 listdata:",this.data.listdata)
   },
   
   onUnload:function(e){
-
-
-    if(this.data.listdata !== ""){
-      // 保存提示
-      console.log("判断存在:listdata",this.data.title)
-      console.log("内容",this.data.content)
-
-      wx.showToast({
-        title: '笔记保存成功',
-        icon: 'success',
-        duration: 2000
-      })
-
-      // 获取用户openid
-      wx.cloud.callFunction({
-        // 云函数名
-        name:"login",
-        // 云函数成功发出请求
-        success:res=>{
-          
-          this.setData({
-            openid:res.result.openid,
+    const that = this
+    console.log("listdata:",that.data.listdata)
+    wx.cloud.callFunction({
+      name:'msgcheck',
+      data:{  
+        content:that.data.listdata
+      } 
+    }).then(ckres=>{ 
+      console.log("审核不通过ckres",ckres)
+    //写审核通过之后的操作 if == 0
+      if (ckres.result.errCode == 0 ||ckres.result.errCode == 44004 ){
+        wx.cloud.callFunction({
+          name: 'msgcheck',
+          data: {
+            content : that.data.listdata
+          },
+          success(res) {
+            console.log("审核通过res",res)
+          }
+        })
+        if(this.data.listdata !== ""){
+          // 保存提示
+          // console.log("判断存在:listdata",this.data.title)
+          // console.log("内容",this.data.content)
+    
+          wx.showToast({
+            title: '笔记保存成功',
+            icon: 'success',
+            duration: 2000
           })
-          console.log("login.openid:",this.data.openid)
- 
+    
+          // 获取用户openid
           wx.cloud.callFunction({
-            name:'createlist',
-            data:{
-              openid:this.data.openid,
-              listdata:this.data.listdata,
-              date:Date.now()
-            },
+            // 云函数名
+            name:"login",
+            // 云函数成功发出请求
             success:res=>{
-              console.log("上传成功",res)
+              
+              this.setData({
+                openid:res.result.openid,
+              })
+              console.log("login.openid:",this.data.openid)
+     
+              wx.cloud.callFunction({
+                name:'createlist',
+                data:{
+                  openid:this.data.openid,
+                  listdata:this.data.listdata,
+                  date:Date.now()
+                },
+                success:res=>{
+                  console.log("上传成功",res)
+                },
+                fail:res=>{
+                  console.log("上传失败:",res)
+                }
+              })
+           
             },
+            // 云函数失败发出请求
             fail:res=>{
-              console.log("上传失败:",res)
+              console.log("login:云函数调用失败")
             }
           })
-       
-        },
-        // 云函数失败发出请求
-        fail:res=>{
-          console.log("login:云函数调用失败")
+        }else{
+          console.log(this.data.content)
+          wx.showToast({
+            title: '已舍弃空白笔记',
+            icon: 'none',
+            duration: 2000
+          })
         }
-      })
-    }else{
-      console.log(this.data.content)
-      wx.showToast({
-        title: '已舍弃空白笔记',
-        icon: 'none',
-        duration: 2000
-      })
-    }
+      }else{
+        // wx.hideLoading(); 
+        
+        wx.showModal({
+          title: '提醒',
+          content: '请注意言论,保存失败',
+          showCancel:false
+        })
+      }
+    })
+
+    
   }
 })
--- pages/add/notepad/notepad.js
+++ pages/add/notepad/notepad.js
@@ -36,69 +36,87 @@ Page({
   // onShow 每次切换页面执行
   // onUnlad 返回触发
   onUnload:function(e){
-
+    const that = this
     console.log("返回触发")
     // title content 其中存在数据 保存
-
-    if(this.data.content !== "" | this.data.title !== ""){
-      // 保存提示
-      console.log("标题",this.data.title)
-      console.log("内容",this.data.content)
-
-      wx.showToast({
-        title: '笔记保存成功',
-        icon: 'success',
-        duration: 2000
-      })
-
-      // 获取用户openid
-      wx.cloud.callFunction({
-        // 云函数名
-        name:"login",
-        // 云函数成功发出请求
-        success:res=>{
-          console.log("login:云函数调用成功")
-          this.setData({
-            openid:res.result.openid,
-          })
-          console.log("openid:",this.data.openid)
-
-          // 保存数据到云数据库
+    wx.cloud.callFunction({
+      name:'msgcheck',
+      data:{
+        content:that.data.content+that.data.title
+      }
+    }).then(ckres=>{
+      console.log("审核不通过ckres",ckres)
+    //写审核通过之后的操作 if == 0
+      if (ckres.result.errCode == 0 ||ckres.result.errCode == 44004  ){
+        if(this.data.content !== "" | this.data.title !== ""){
+          // 保存提示
           console.log("标题",this.data.title)
           console.log("内容",this.data.content)
-        
-          wx.cloud.callFunction({
-            name:'createnotepad',
-            data:{ 
-              title:this.data.title,
-              content:this.data.content,
-              openid:this.data.openid,
-              date:Date.now()
-            },
+    
+          wx.showToast({
+            title: '笔记保存成功',
+            icon: 'success',
+            duration: 2000
           })
+    
+          // 获取用户openid
+          wx.cloud.callFunction({
+            // 云函数名
+            name:"login",
+            // 云函数成功发出请求
+            success:res=>{
+              console.log("login:云函数调用成功")
+              this.setData({
+                openid:res.result.openid,
+              })
+              console.log("openid:",this.data.openid)
+    
+              // 保存数据到云数据库
+              console.log("标题",this.data.title)
+              console.log("内容",this.data.content)
+            
+              wx.cloud.callFunction({
+                name:'createnotepad',
+                data:{ 
+                  title:this.data.title,
+                  content:this.data.content,
+                  openid:this.data.openid,
+                  date:Date.now()
+                },
+              })
+              // wx.switchTab({
+              //   url:"../../pages/file/file"
+              // })
+            },
+            // 云函数失败发出请求
+            fail:res=>{
+              console.log("login:云函数调用失败")
+            }
+          })
+    
+          
+        }else{
+          console.log(this.data.content)
+          wx.showToast({
+            title: '已舍弃空白笔记',
+            icon: 'none',
+            duration: 2000
+          })
+    
           // wx.switchTab({
           //   url:"../../pages/file/file"
           // })
-        },
-        // 云函数失败发出请求
-        fail:res=>{
-          console.log("login:云函数调用失败")
         }
-      })
-
-      
-    }else{
-      console.log(this.data.content)
-      wx.showToast({
-        title: '已舍弃空白笔记',
-        icon: 'none',
-        duration: 2000
-      })
-
-      // wx.switchTab({
-      //   url:"../../pages/file/file"
-      // })
-    }
+      }else{
+        // wx.hideLoading();
+        wx.showModal({
+          title: '提醒',
+          content: '请注意言论,保存失败',
+          showCancel:false
+        })
+      }
+    })
+    
      
   }
 
